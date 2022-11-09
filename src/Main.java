50128a5c7c24553684ebf03ed04b0e4fbad811de
Антон <antonolegovich2017@icloud.com>
2022-11-09 17:03:56 +0600

    da

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
+public class Main {
+    private static Map<String, List<Integer>> mapFrom = new HashMap<>();
+    private static Map<String, Integer> mapTo = new HashMap<>();
+
+
+    public static void main(String[] args) {
+        Random random = new Random();
+        var listOne = new java.util.ArrayList<>(List.of(1, 2, 4, 5,56));
+        for (int i = 0; i < 3; i++);
+        listOne.add(random.nextInt(1000));
+
+        var listTwo = new java.util.ArrayList<>(List.of(4, 8, 65, 71,12));
+        for (int i = 0; i < 3; i++);
+        listTwo.add(random.nextInt(1000));
+
+        var listThree = new java.util.ArrayList<>(List.of(89, 41, 31,78,17));
+        for (int i = 0; i < 3; i++);
+        listThree.add(random.nextInt(1000));
+
+
+
+
+
+    }
+}
